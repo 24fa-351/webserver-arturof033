13d3c6958f13c953511396f7f0a81f09bd254acc
Arturo Flores <68297760+arturof033@users.noreply.github.com>
2024-11-24 19:54:26 -0800

    Update http_message.h

--- http_message.h
+++ http_message.h
@@ -9,7 +9,9 @@
 #define HTTP_MESSAGE_H
 
 #define MIN_BUFFER_LENGTH 10
-#define MAX_RESPONSE_SIZE 100
+#define MAX_BUFFER_LENGTH 1024
+#define MAX_COMMAND_RESPONSE_SIZE 100
+#define MAX_HTTP_CLIENT_RESPONSE_SIZE 2048
 #define SEPARATOR_LENGTH 30
 #define ERROR_MESSAGE "\n-------------------\nENTER VALID COMMAND\n-------------------\n\n"
 #define SEPARATOR "----------------------------\n\n"
@@ -19,6 +21,7 @@ typedef struct msg{
     char* method;
     char* path;
     char* http_version;
+    char* host;
     char* body;
     int body_length;
     char* headers;
@@ -34,10 +37,7 @@ typedef enum{
     BAD_REQUEST,
     CLOSED_CONNECTION,
     INVALID_COMMAND,
-    MESSAGE, 
-    STATIC,
-    STATS,
-    CALC
+    MESSAGE
 } http_read_result_t;
 
 // responses: a message, bad request, closed connection
@@ -46,6 +46,14 @@ void read_http_client_message(int client_sock,
         http_client_message_t** msg,
         http_read_result_t* result,
         server_info** http_info);
+
+char* get_response_to_http_client_message(http_client_message_t* http_msg, server_info* http_info);
+
+void fill_http_client_message(http_client_message_t** http_msg, server_info** http_info);
+
+void print_http_client_message(int client_fd, http_client_message_t*http_msg);
+
+void initialize_http_info(server_info** http_info);
 
 void http_client_message_free(http_client_message_t* msg);
 
